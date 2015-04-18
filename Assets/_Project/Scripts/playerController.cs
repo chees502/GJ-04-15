241a9c37e88a13f5844c722eec0117bf5b8b698c
Brandon Farrell <chees502@gmail.com>
2015-04-17 21:24:25 -0700

    basic controller

---
+++ Assets/_Project/Scripts/playerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+	Vector2 velocity;
+	public float friction=1;
+	public float runSpeed=1;
+	void Update(){
+		if(Input.GetKey("a")){
+			Move (-1);
+		}
+		if(Input.GetKey("d")){
+			Move (1);
+		}
+		Phys();
+	}
+	void Move(float dir){
+		Debug.Log (dir);
+		velocity.x+=dir*runSpeed;
+	}
+	void Phys(){
+		Vector3 newPos = transform.position;
+		newPos.x+=velocity.x*Time.deltaTime;
+		newPos.y+=velocity.y*Time.deltaTime;
+		transform.position=newPos;
+		velocity-=velocity*Time.deltaTime*friction;
+	}
+}
